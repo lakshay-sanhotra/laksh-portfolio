af01bd5027b1381bf72ef29e55c7c3d248348b48
LakshaySanhotra <lakshaysanhotra1@gmail.com>
2024-01-20 21:44:29 +0530

    home navigate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,7 @@ function Header() {
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ms-auto flex-lg-column text-center ">
                 <li className="nav-item">
-                  <a className="nav-link" href="/">
+                  <a className="nav-link" href="#home">
                     Home
                   </a>
                 </li>
