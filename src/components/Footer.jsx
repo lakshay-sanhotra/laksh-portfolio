3899445c6165cdea322e2a9afa577ed3b143f4b3
LakshaySanhotra <lakshaysanhotra1@gmail.com>
2024-06-13 20:20:02 +0530

    website update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,12 @@ import React from 'react'
 function Footer() {
   return (
     <>
-        <footer className="py-5 px-lg-5">
+        <footer className="py-3 px-lg-5">
                 <div className="container">
-                    <div className="row gy-4 justify-content-between">
+                    <div className="row gy-4 justify-content-between align-items-center">
                         <div className="col-auto">
-                            <p className="mb-0">Design by <a target="_blank" href="https://www.youtube.com/@SA7MAN"
-                                    className="fw-bold">SA7MAN</a> <br/>
-                                    Developed by <a target="_blank" href="https://www.instagram.com/mrcoder_xd/"
-                                    className="fw-bold text-brand">mrcoder_xd</a></p>
+                            <p className="mb-0"><a target="_blank" href="https://www.instagram.com/lakshay_11nov/"
+                                    className="fw-bold text-brand">Lakshay</a></p>
                         </div>
                         <div className="col-auto">
                             <div className="social-icons">
