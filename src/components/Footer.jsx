d5b337058adbd7087b0d22d88994869a0a5e78e7
LakshaySanhotra <lakshaysanhotra1@gmail.com>
2025-06-22 16:14:00 +0530

    image change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,15 @@ function Footer() {
     <>
         <footer className="py-3 px-lg-5">
                 <div className="container">
-                    <div className="row gy-4 justify-content-between align-items-center">
-                        <div className="col-auto">
+                    <div className="row gy-4 justify-content-center align-items-center">
+                        {/* <div className="col-auto">
                             <p className="mb-0">Design by: <a target="_blank" href="https://www.instagram.com/lakshay_11nov/"
                                     className="fw-bold text-brand">Lakshay</a></p>
-                        </div>
+                        </div> */}
                         <div className="col-auto">
                             <div className="social-icons">
-                                <a target="_blank" href="https://www.instagram.com/lakshay_sanhotra/"><i className="lab la-instagram"></i></a>
+                                <a target="_blank" href="https://www.instagram.com/1lakshay2004/"><i className="lab la-instagram"></i></a>
+                                &nbsp;&nbsp;
                                 <a target="_blank" href="https://github.com/lakshay-sanhotra"><i className="lab la-github"></i></a>
                             </div>
                         </div>
